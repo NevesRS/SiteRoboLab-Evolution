b44d6a28957d3b39dac37526b16fa4341fd8aaf2
NevesRS <88750273+NevesRS@users.noreply.github.com>
2022-09-28 07:21:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();                 /* npm i --s ejs */
+
+app.use(express.static(__dirname + '/views'));
+
+app.listen(3000, function(){
+  console.log("Servidor no ar - Porta: 3000!")
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/views/home.html');
+});
